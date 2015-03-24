4e3bcb5e144fcd27a24c723e7c6ad345bce930af
Nick Jones <nick@tinygoatstudio.com>
2015-03-24 00:15:27 -0400

    - Initial commit

---
+++ gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig ({
+    sass: {
+      dist: {
+        files: {
+          'public/stylesheets/style.css' : 'sass/style.scss'
+        }
+      }
+    },
+    watch: {
+      source: {
+        files: ['sass/**/*.scss','views/*.ejs'],
+        tasks: ['sass'],
+        options: {
+          livereload: true, // needed to run LiveReload
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['sass']);
+
+};
