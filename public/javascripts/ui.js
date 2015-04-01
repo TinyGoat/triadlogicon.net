7c9b74eeee27159d9cce6d6361c4019568086ab3
Nick Jones <nick@tinygoatstudio.com>
2015-04-01 00:36:16 -0400

    - Implements lots of responsive stuff - Implements nice RWD nav

--- public/javascripts/ui.js
+++ public/javascripts/ui.js
@@ -1,3 +1,8 @@
 jQuery(document).ready(function($) {
 	$('body').stellar();
+
+	$('#mobile-nav-trigger').click(function(){
+		console.log("Click...");
+		$('#mobile-nav').slideToggle(200);
+	});
 });
